e2ff35090bf48f51cc5225cf494ea8591c794eda
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-01-13 13:01:09 -0500

    secondary container allignment fix

--- src/components/VedioTitle.js
+++ src/components/VedioTitle.js
@@ -6,7 +6,9 @@ const VedioTitle = ({ title, overview }) => {
 	return (
 		<div className='pt-[25%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black w-screen aspect-video'>
 			<h1 className=' text-2xl md:text-6xl font-bold'>{title}</h1>
-			<p className='hidden md:inline-block py-6 text-lg w-1/4'>{overview}</p>
+			<p className='hidden md:inline-block py-6 text-lg w-1/4'>
+				{overview.slice(0, 100).concat(overview.length > 100 ? '...' : '')}
+			</p>
 			<div className='my-4 md:m-0'>
 				<button className='bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl hover:bg-opacity-80 rounded-md'>
 					▶️ Play
